c76f318356005449dd9b04e62a1380515c5dd10d
neshtosh <aleckogitonga8@gmail.com>
2025-06-04 16:13:43 +0300

    hero

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -201,7 +201,7 @@ const Home: React.FC = () => {
             initial={{ opacity: 0 }}
             animate={{ opacity: 1 }}
             transition={{ delay: 1.2, duration: 1 }}
-            className="absolute -bottom-20 md:-bottom-60 left-1/2 -translate-x-1/2"
+            className="absolute -bottom-60 md:-bottom-60 left-1/2 -translate-x-1/2"
           >
             <a 
               href="#featured-work"
